5c8303284eb04190f4d61273a929e425baa7eabf
pizzadeesnutz <arcadiomj@yahoo.com>
2016-02-29 03:48:59 -0500

    90% There
    For some reason the damage on Enemy_4 isn't right. I can shoot right at
    it and the projectiles will mostly go through, some will actually hit.
    Can't figure out why, the code looks perfect so I'm thinking there is
    something between the super and sub class that isn't meshing right.

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -1,15 +1,138 @@
 ﻿using UnityEngine;
 using System.Collections;
+[System.Serializable]
+public class Part {
+	public string name; 
+	public float health; 
+	public string[] protectedBy; 
 
-public class Enemy_4 : MonoBehaviour {
+	public GameObject go;
+	public Material mat; 
+}
+public class Enemy_4 : Enemy {
+	public Vector3[] points; 
+	public float timeStart; 
+	public float duration = 4; 
 
-	// Use this for initialization
+	public Part[] parts; 
+
 	void Start () {
-	
+		points = new Vector3[2];
+		points[0] = pos;
+		points[1] = pos;
+
+		InitMovement();
+		Transform t;
+		foreach(Part prt in parts) {
+			t = transform.Find(prt.name);
+			if (t != null) {
+				prt.go = t.gameObject;
+				prt.mat = prt.go.GetComponent<Renderer>().material;
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void InitMovement() {
+		Vector3 p1 = Vector3.zero;
+		float esp = Main.S.enemySpawnPadding;
+		Bounds cBounds = Utils.camBounds;
+		p1.x = Random.Range(cBounds.min.x + esp, cBounds.max.x - esp);
+		p1.y = Random.Range(cBounds.min.y + esp, cBounds.max.y - esp);
+
+		points[0] = points[1];
+		points[1] = p1;
+
+		timeStart = Time.time;
+	}
+	public override void Move () {
+		float u = (Time.time-timeStart)/duration;
+		if (u>=1) { 
+			InitMovement();
+			u=0;
+		}
+
+		u = 1 - Mathf.Pow( 1-u, 2 );
+		pos = (1-u)*points[0] + u*points[1];
+	}
+		
+	void OnCollisionEnter( Collision coll ) {
+		GameObject other = coll.gameObject;
+		switch (other.tag) {
+		case "ProjectileHero":
+			Projectile p = other.GetComponent<Projectile>();
+			bounds.center = transform.position + boundsCenterOffset;
+			if (bounds.extents == Vector3.zero || Utils.ScreenBoundsCheck(bounds,BoundsTest.offScreen) != Vector3.zero) {
+				Destroy(other);
+				break;
+			}//end of if
+
+			GameObject goHit = coll.contacts[0].thisCollider.gameObject;
+			Part prtHit = FindPart(goHit);
+			if (prtHit == null) { 
+				goHit = coll.contacts[0].otherCollider.gameObject;
+				prtHit = FindPart(goHit);
+			}//end of if
+			if (prtHit.protectedBy != null) {
+				foreach( string s in prtHit.protectedBy ) {
+					if (!Destroyed(s)) {
+						Destroy(other); 
+						return;
+					}//end of if
+				}//end of foreach
+			}//end of if
+			prtHit.health -= Main.W_DEFS[p.type].damageOnHit;
+			ShowLocalizedDamage(prtHit.mat);
+			if (prtHit.health <= 0) {
+				prtHit.go.SetActive(false);
+			}//end of if
+			bool allDestroyed = true; 
+			foreach( Part prt in parts ) {
+				if (!Destroyed(prt)) {
+					allDestroyed = false; 
+					break;
+				}//end of if
+			}//end of foreach
+			if (allDestroyed) {
+				Main.S.ShipDestroyed( this );
+				Destroy(this.gameObject);
+			}//end of if
+			Destroy(other);
+			break;
+		}//end of switch
+	}//end of Collision
+
+	Part FindPart(string n) {
+		foreach( Part prt in parts ) {
+			if (prt.name == n) {
+				return( prt );
+			}
+		}
+		return( null );
+	}
+	Part FindPart(GameObject go) {
+		foreach( Part prt in parts ) {
+			if (prt.go == go) {
+				return( prt );
+			}
+		}
+		return( null );
+	}
+
+	bool Destroyed(GameObject go) {
+		return( Destroyed( FindPart(go) ) );
+	}
+	bool Destroyed(string n) {
+		return( Destroyed( FindPart(n) ) );
+	}
+	bool Destroyed(Part prt) {
+		if (prt == null) {
+			return(true); 
+		}
+		return (prt.health <= 0);
+	}
+
+	void ShowLocalizedDamage(Material m) {
+		m.color = Color.red;
+		remainingDamageFrames = showDamageForFrames;
 	}
 }
